fd0b368fa00b2e8af0a546119511bc15b79089c3
HuggoPacobahyba <huggo9@hotmail.com>
2024-06-20 20:02:02 -0300

    Tela Inicial EditText

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,28 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val edtPeso = findViewById<TextInputEditText>(R.id._edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btn_Calcular = findViewById<Button>(R.id.btn_calcular)
+
+
+        btn_Calcular.setOnClickListener(){
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+            println("Huggo " + peso + altura)
+
+        }
+
+
     }
 }
